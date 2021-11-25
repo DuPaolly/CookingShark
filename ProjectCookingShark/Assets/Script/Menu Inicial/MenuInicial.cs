59d5e69a90ad9fe98d40127fcf8e96905b684a48
Eduarda Paolly <paollyk13@outlook.com>
2021-11-25 17:22:32 -0300

    UI Assets imported and MenuStart created
    The current assets of UI Has been imported

--- ProjectCookingShark/Assets/Script/Menu Inicial/MenuInicial.cs
+++ ProjectCookingShark/Assets/Script/Menu Inicial/MenuInicial.cs
@@ -8,24 +8,32 @@ public class MenuInicial : MonoBehaviour
 {
     [SerializeField] GameObject MenuOptions;
     [SerializeField] GameObject MenuPerguntaSair;
+    [SerializeField] GameObject menuStart;
+    [SerializeField] GameObject menuInicial;
     private void Start()
     {
         MenuOptions.SetActive(false);
         MenuPerguntaSair.SetActive(false);
+        menuInicial.SetActive(false);
+    }
+
+    private void Update()
+    {
+        PressSpace();
     }
     public void BotaoJogar()
     {
         Debug.Log("Iniciar o Jogo");
-        //SceneManager.LoadScene("Jogo");
+        SceneManager.LoadScene("Jogo");
         //spritePrato.sprite = SpriteReceitas[1];
     }
 
-    public void BotaoHistoria()
-    {
-        Debug.Log("Iniciar a história do Jogo");
-        ///spritePrato.sprite = SpriteReceitas[0];
+    //public void BotaoHistoria()
+    //{
+    //    Debug.Log("Iniciar a história do Jogo");
+    //    ///spritePrato.sprite = SpriteReceitas[0];
 
-    }
+    //}
     public void BotaoOpcoes()
     {
         Debug.Log("Abre o menu de opções");
@@ -37,5 +45,12 @@ public class MenuInicial : MonoBehaviour
         Debug.Log("Fechar o jogo");
     }
 
-
+    void PressSpace()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            menuStart.SetActive(false);
+            menuInicial.SetActive(true);
+        }
+    }
 }
