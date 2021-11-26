47612a51565a0c6a9ce11ff53114bcbd96a42f27
GeovaneLJunior <geovanedesenha@gmail.com>
2021-11-25 21:17:03 -0400

    The Logo animation is working and prefabs already been created

--- ProjectCookingShark/Assets/Script/JogoUI/UIMenuStartMoviment.cs
+++ ProjectCookingShark/Assets/Script/JogoUI/UIMenuStartMoviment.cs
@@ -5,84 +5,264 @@ using UnityEngine;
 public class UIMenuStartMoviment : MonoBehaviour
 {
 
-    [SerializeField] float rotacaoDoBackground = 1;
+    [SerializeField] float velocidadeDeRotacao = 1;
+
+    [SerializeField] float VelocidadeDeBoiar = 1;
 
     [SerializeField] float velocidadeY = 1;
 
     [SerializeField] float alturaLimite = 1;
 
+    [SerializeField] float rotacaoLimite = 1;
+
     [SerializeField] private thisObjectIs tipoDeObjeto;
 
-    [SerializeField] private Positions posicaoDeSpawnDasBolhas;
+    [SerializeField] private Positions posicaoDeSpawnDaLogo;
 
-    private float escalaInicial = 2;
+    [SerializeField] private Positions posicaoFinalDeSpawnDaLogo;
+
+    [SerializeField] private ParticleSystem particula01;
+
+    [SerializeField] private ParticleSystem particula02;
+
+    [SerializeField] private ParticleSystem particula03;
+
+    //[SerializeField] private Positions boiaDeCima;
+
+    //[SerializeField] private Positions boiaDeBaixo;
+
+    [SerializeField] [Range(0, 1)] float smoothVelocidade = 1;
+
+    private Positions detectarPosition;
+
+    private estadoDaLogo estadoAtualDaLogo = estadoDaLogo.entrando;
 
     private bool paraCima = true;
 
+    private bool girandoSentidoHorario = false;
+
     private Vector3 _originalPosition;
 
+    private Vector2 offset, _posicaoAtual;
+
     float rotacaoAtual = 0;
+
+    float tempoDoEfetoEmTela = 4;
+
+    float tempoAtualDoEfeito = 0;
+
+    bool  pararAContagem = false;
+
+    enum estadoDaLogo
+    {
+        entrando,
+        saindo,
+        parado
+    }
 
     enum thisObjectIs
     {
         Boat,    
         BackGround,
-        Bubble
+        Logo
     }
 
     void Awake()
     {
         ParametrosIniciaisDoObjeto();
+
+       
     }
-    void Update()
+    void FixedUpdate()
     {
-        RotacioneEsteObjeto();
+        AnimeEsteObjeto();
+        MudarEstadoDaLogo();
     }
 
-    private void RotacioneEsteObjeto()
+    private void OnTriggerEnter2D(Collider2D areaEmQueEncostou)
+    {
+        Positions localAchado = areaEmQueEncostou.GetComponent<Positions>();
+
+        if (localAchado != null)
+        {
+            detectarPosition = localAchado;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D areaEmQueSaiu)
+    {
+        Positions localAchado = areaEmQueSaiu.GetComponent<Positions>();
+
+        if (localAchado != null)
+        {
+            detectarPosition = null;
+        }
+    }
+
+    private void AnimeEsteObjeto()
     {
         if (tipoDeObjeto == thisObjectIs.BackGround)
         {
-            rotacaoDoObjeto(rotacaoDoBackground);
+            RotacaoDoObjeto(velocidadeDeRotacao);
         }
         if (tipoDeObjeto == thisObjectIs.Boat)
         {
-            if (paraCima)
-            {
-                if(transform.position.y >= _originalPosition.y + alturaLimite)
-                {
-                    paraCima = false;
-                }
-                MoverParaCima(velocidadeY);
-            }
-            else if (!paraCima)
-            {
-                if (transform.position.y <= _originalPosition.y - alturaLimite)
-                {
-                    paraCima = true;
-                }
-                MoverParaBaixo(velocidadeY);
-            }
+            MoverBarco();
         }
-        if (tipoDeObjeto == thisObjectIs.Bubble)
+        if (tipoDeObjeto == thisObjectIs.Logo)
         {
-            MoverBolha();
+            MoverEBoiar();
         }
     }
     
-    private void rotacaoDoObjeto(float velocidadeDeGiro)
+    private void RotacaoDoObjeto(float velocidadeDeGiro)
     {
         rotacaoAtual += velocidadeDeGiro;
 
         transform.rotation = Quaternion.Euler(0, 0, rotacaoAtual);
     }
-
-    private void MoverBolha()
+    private void MoverEBoiar()
     {
-        MoverParaEsquerda(velocidadeY);
-        MoverParaCima(velocidadeY);
+        if (estadoAtualDaLogo == estadoDaLogo.parado)
+        {
+            PararEfeitoDeParticula();
+            //MoverLogo();
+            //BoiarObjeto(VelocidadeDeBoiar);
+        }
+        else if (estadoAtualDaLogo == estadoDaLogo.entrando)
+        {
+            VaParaPosicao(posicaoFinalDeSpawnDaLogo);
+            //BoiarObjeto(VelocidadeDeBoiar);
+        }
+        else if (estadoAtualDaLogo == estadoDaLogo.saindo)
+        {
+            VaParaPosicao(posicaoDeSpawnDaLogo);
+            //BoiarObjeto(VelocidadeDeBoiar);
+        }
     }
 
+    private void PararEfeitoDeParticula()
+    {
+        if (!pararAContagem)
+        {
+            tempoAtualDoEfeito += Time.deltaTime;
+        }
+        if(tempoDoEfetoEmTela <= tempoAtualDoEfeito)
+        {
+            particula01.Stop();
+            particula02.Stop();
+            particula03.Stop();
+            pararAContagem = true;
+        }      
+    }
+
+    private void MoverBarco()
+    {
+        if (paraCima)
+        {
+            if (transform.position.y >= _originalPosition.y + alturaLimite)
+            {
+                paraCima = false;
+            }
+            MoverParaCima(velocidadeY);
+        }
+        else if (!paraCima)
+        {
+            if (transform.position.y <= _originalPosition.y - alturaLimite)
+            {
+                paraCima = true;
+            }
+            MoverParaBaixo(velocidadeY);
+        }
+    }
+
+    //private void MoverLogo()
+    //{
+    //    if (paraCima)
+    //    {
+    //        //Debug.Log("Esta Passando por aqui");
+    //        if (transform.position.y > boiaDeCima.transform.position.y - 0.01)
+    //        {
+    //            paraCima = false;
+    //        }
+    //        VaParaPosicao(boiaDeCima);
+    //    }
+    //    else if (!paraCima)
+    //    {
+    //        if (transform.position.y < boiaDeBaixo.transform.position.y + 0.01)
+    //        {
+    //            paraCima = true;
+    //        }
+    //        VaParaPosicao(boiaDeBaixo);
+    //    }
+    //}
+
+    private void MudarEstadoDaLogo()
+    {
+        if (estadoAtualDaLogo != estadoDaLogo.saindo)
+        {
+            if (detectarPosition == posicaoFinalDeSpawnDaLogo)
+            {
+                estadoAtualDaLogo = estadoDaLogo.parado;
+            }else if(posicaoFinalDeSpawnDaLogo == null)
+            {
+                Debug.Log("ERROR");
+            }
+        }else if (estadoAtualDaLogo == null)
+        {
+            Debug.Log("ERROR");
+        }
+    }
+
+    public void SairLogo()
+    {
+        estadoAtualDaLogo = estadoDaLogo.saindo;
+        particula01.Play();
+        particula02.Play();
+        particula03.Play();
+    }
+
+    //transform.position.y <= posicaoFinalDeSpawnDaLogo.transform.position.y + 0.01
+    //            && transform.position.y >= posicaoFinalDeSpawnDaLogo.transform.position.y - 0.01
+
+    //private void BoiarObjeto(float velocidadeDeGiro)
+    //{
+    //    if (tipoDeObjeto == thisObjectIs.Logo)
+    //    {
+    //        if (girandoSentidoHorario)
+    //        {
+    //            if (transform.rotation.z >= 0 + rotacaoLimite)
+    //            {
+    //                girandoSentidoHorario = false;
+    //            }
+    //            RotacaoDoObjeto(velocidadeDeGiro);
+    //        }
+    //        else if (!girandoSentidoHorario)
+    //        {
+    //            if (transform.rotation.z <= 0 - rotacaoLimite)
+    //            {
+    //                girandoSentidoHorario = true;
+    //            }
+    //            RotacaoDoObjeto(-velocidadeDeGiro);
+    //        }
+    //    }
+    //}
+
+    public void VaParaPosicao(Positions posicao)
+    {
+
+        _originalPosition = posicao.transform.position;
+
+        _posicaoAtual = transform.position;
+
+        _posicaoAtual = Vector3.Lerp(
+            transform.position,
+            _originalPosition,
+            smoothVelocidade * Time.deltaTime);
+
+        transform.position = _posicaoAtual;
+
+    }
 
     private void MoverParaCima(float velocidade)
     {
@@ -99,21 +279,20 @@ public class UIMenuStartMoviment : MonoBehaviour
 
     private void ParametrosIniciaisDoObjeto()
     {
-        rotacaoDoBackground = rotacaoDoBackground / 100;
+        velocidadeDeRotacao = velocidadeDeRotacao / 100;
         rotacaoAtual = 0;
         _originalPosition = transform.position;
         alturaLimite = alturaLimite / 100;
-        startBolha();
+        startLogo();
     }
 
-    private void startBolha()
+    private void startLogo()
     {
-        if(tipoDeObjeto == thisObjectIs.Bubble)
+        if(tipoDeObjeto == thisObjectIs.Logo)
         {
-            escalaInicial = escalaInicial / 100;
-            transform.position = posicaoDeSpawnDasBolhas.transform.position;
-            _originalPosition = posicaoDeSpawnDasBolhas.transform.position;
-            transform.localScale = new Vector3(escalaInicial, escalaInicial, escalaInicial);
+            estadoAtualDaLogo = estadoDaLogo.entrando;
+            transform.position = posicaoDeSpawnDaLogo.transform.position;
+            _originalPosition = posicaoDeSpawnDaLogo.transform.position;
         }
     }
     public int SortearNumero(float[] numeros)
